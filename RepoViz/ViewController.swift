0019968b42104d341caaee548d2719c4cac82128
LIam Butler-Lawrence <liam@gotruemotion.com>
2019-06-18 18:22:51 -0400

    Initial Commit

---
+++ RepoViz/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RepoViz
+//
+//  Created by Liam Butler-Lawrence on 6/18/19.
+//  Copyright © 2019 TrueMotion. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
